7e3e2c47cf417821773346ccdb6bb5905275ea16
章丹 <zhangdan@lqwydeiMac.local>
2019-08-14 17:20:05 +0800

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux'
 import './index.css';
 
 ReactDOM.render(
@@ -10,6 +9,9 @@ ReactDOM.render(
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
+
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 function user(state = {name: 'redux'}, action: { type: any; name: any; }) {
   switch (action.type) {
@@ -35,14 +37,16 @@ const rootReducer = combineReducers({
   project,
   user
 })
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer) 
+const finalCreateStore = compose(applyMiddleware(thunk))(createStore)  // applyMiddlewares  是 Redux 的原生方法，作用是将所有中间件组成一个数组，依次执行。
+const store = finalCreateStore(rootReducer, {})
 
 function render(state = store.getState()) {
   const $userName = document.getElementById('userName') as HTMLInputElement
   $userName.innerHTML = state.user.name
 }
 render()
-// console.log(store.getState())
+console.log(store.getState())
 
 
 store.subscribe(() => {
@@ -54,8 +58,17 @@ const userNameButton = document.getElementById('userNameButton') as HTMLInputEle
 userNameButton.onclick = (() => {
   const value = $userNameInput.value
   console.log(value)
-  store.dispatch({
-    name: value,
-    type: 'CHANGE_NAME'
-  })
+  // store.dispatch({ 
+  //   name: value,
+  //   type: 'CHANGE_NAME'
+  // })
+  // redux-thunk的作用就是让dispatch方法不仅仅只接收action对象，还可以包含一个方法。我们可以在这个方法内部去调用异步代码
+  store.dispatch<any>((dispatch: (arg0: { name: string; type: string; }) => void, getState: any) => {
+    setTimeout(() => {
+      dispatch({
+        name: value,
+        type: 'CHANGE_NAME'
+      })
+    }, 2000)
+  }) // 可以看到页面在两秒之后才发生变化
 })
